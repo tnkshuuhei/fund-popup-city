b35d2fb4d99ac91ebe7b02d38e366a5340ba0efa
shuhei tanaka <shuhei46491@gmail.com>
2024-11-06 19:24:12 +0700

    chore: rm submit from nav

--- components/global/nav-links.tsx
+++ components/global/nav-links.tsx
@@ -8,67 +8,67 @@ import { usePathname } from "next/navigation";
 import type { ReactNode } from "react";
 
 export const NavLink = ({
-	href,
-	children,
-	isActive,
-	className,
+  href,
+  children,
+  isActive,
+  className,
 }: {
-	href: string;
-	children: ReactNode;
-	isActive: boolean;
-	className?: string;
+  href: string;
+  children: ReactNode;
+  isActive: boolean;
+  className?: string;
 }) => {
-	return (
-		<Link
-			href={href}
-			className={cn(
-				buttonVariants({ variant: "ghost" }),
-				"rounded-md font-semibold",
-				isActive ?? "bg-vd-beige-300 dark:bg-vd-beige-100",
-				className || "",
-			)}
-		>
-			{children}
-		</Link>
-	);
+  return (
+    <Link
+      href={href}
+      className={cn(
+        buttonVariants({ variant: "ghost" }),
+        "rounded-md font-semibold",
+        isActive ?? "bg-vd-beige-300 dark:bg-vd-beige-100",
+        className || ""
+      )}
+    >
+      {children}
+    </Link>
+  );
 };
 
 const NavLinks = () => {
-	const pathname = usePathname();
-	const isActive = (path: string) => pathname.startsWith(path);
+  const pathname = usePathname();
+  const isActive = (path: string) => pathname.startsWith(path);
 
-	return (
-		<ul className="hidden gap-1 md:flex">
-			<li>
-				<NavLink href="/" isActive={isActive("/reports")}>
-					Browse
-				</NavLink>
-			</li>
-			<li>
+  return (
+    <ul className="hidden gap-1 md:flex">
+      <li>
+        <NavLink href="/" isActive={isActive("/reports")}>
+          Browse
+        </NavLink>
+      </li>
+      {/* <li>
 				<NavLink href="/submit" isActive={isActive("/submit")}>
 					Submit
 				</NavLink>
-			</li>
-			<li>
-				<a
-					href="https://testnet.hypercerts.org/docs/intro"
-					target="_blank"
-					rel="noopener noreferrer"
-					className={cn(
-						buttonVariants({ variant: "link" }),
-						"group font-semibold",
-					)}
-				>
-					<span>FAQs</span>
-					<ArrowUpRight
-						size={18}
-						className="group-hover:-translate-y-0.5 ml-1 opacity-70 transition-transform duration-300 ease-in-out group-hover:translate-x-0.5 group-hover:opacity-100"
-						aria-hidden="true"
-					/>
-				</a>
-			</li>
-		</ul>
-	);
+			</li> */}
+      <li>
+        <a
+          href="https://testnet.hypercerts.org/docs/intro"
+          target="_blank"
+          rel="noopener noreferrer"
+          className={cn(
+            buttonVariants({ variant: "link" }),
+            "group font-semibold"
+          )}
+        >
+          <span>FAQs</span>
+          <ArrowUpRight
+            size={18}
+            className="group-hover:-translate-y-0.5 ml-1 opacity-70 transition-transform duration-300 ease-in-out group-hover:translate-x-0.5 group-hover:opacity-100"
+            aria-hidden="true"
+          />
+        </a>
+      </li>
+    </ul>
+  );
 };
 
 NavLinks.displayName = "NavLinks";
--- components/global/nav-menu.tsx
+++ components/global/nav-menu.tsx
@@ -7,31 +7,31 @@ import { NavLinks } from "@/components/global/nav-links";
 import { WalletProfile } from "@/components/global/wallet-profile";
 
 const NavMenu = () => {
-	return (
-		<nav className="border-b-[1.5px] border-b-stone-300 py-2 md:py-5">
-			<section className="container flex max-w-7xl items-center justify-between">
-				<div className="flex flex-1 items-center py-5 md:absolute md:left-[50%]">
-					<Link
-						className="flex w-full justify-center"
-						aria-label="ZuDeck Home"
-						href="/reports"
-					>
-						<Logo
-							className="h-8 w-auto"
-							alt="Hypercerts Logo"
-							width={250}
-							height={250}
-						/>
-					</Link>
-				</div>
-				<NavLinks />
+  return (
+    <nav className="border-b-[1.5px] border-b-stone-300 py-2 md:py-5">
+      <section className="container flex max-w-7xl items-center justify-between">
+        <div className="flex flex-1 items-center py-5 md:absolute md:left-[50%]">
+          <Link
+            className="flex w-full justify-center"
+            aria-label="ZuDeck Home"
+            href="/"
+          >
+            <Logo
+              className="h-8 w-auto"
+              alt="Hypercerts Logo"
+              width={250}
+              height={250}
+            />
+          </Link>
+        </div>
+        <NavLinks />
 
-				<div className="hidden gap-2 md:flex">
-					<WalletProfile />
-				</div>
-			</section>
-		</nav>
-	);
+        <div className="hidden gap-2 md:flex">
+          <WalletProfile />
+        </div>
+      </section>
+    </nav>
+  );
 };
 
 NavMenu.displayName = "NavMenu";
